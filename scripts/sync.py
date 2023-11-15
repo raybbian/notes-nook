9c04e491179cbd69984c233071d73e7732387596
Raymond Bian <raybbian@gmail.com>
2023-11-15 14:07:02 -0500

    upd

--- scripts/sync.py
+++ scripts/sync.py
@@ -66,17 +66,54 @@ class Sync:
                 raise Exception(f"Failed to get {note_name} from {course_name}.")
             return response.text
 
-    async def save_notes_to_json(self):
-        courses = await self.get_course_names()
-        out = {}
-        out['timestamp'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        out['courses'] = {}
-        for course_name in courses:
-            out['courses'][course_name] = {}
-            note_names = await self.get_note_names(course_name)
-            for note_name in note_names:
-                note_content = await self.get_note_content(course_name, note_name)
-                out['courses'][course_name][note_name] = note_content
 
-        with open('../notes.json', 'w') as file:
-            json.dump(out, file, indent=4)
+    async def get_figure_names(self, course_name):
+        url = BASE_URL + SEMESTER_DIR + f"/{course_name}/figures:/children"
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url, headers=self.headers)
+            if response.status_code != 200:
+                raise Exception(f"Failed to get {course_name} figures.")
+            out = []
+            for item in json.loads(response.content)["value"]:
+                if "file" in item:
+                    out.append(item["name"])
+            return out
+
+    async def get_figure_content(self, course_name, figure_name):
+        url = BASE_URL + SEMESTER_DIR + f"/{course_name}/figures/{figure_name}:/content"
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url, headers=self.headers)
+            if response.status_code == 302:
+                response = await client.get(response.headers['location'])
+            elif response.status_code != 200:
+                raise Exception(f"Failed to get {figure_name} from {course_name}.")
+            return response.content
+
+    async def save_notes_to_disk(self):
+        courses = await self.get_course_names()
+        for course_name in courses:
+            print(f"Processing course {course_name}...")
+            with open(f'../notes/{course_name}/{course_name}.tex', 'w') as file:
+                lines = [
+                    fr'% updated {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}',
+                    r'\documentclass[a4paper]{article}',
+                    r'\input{../preamble.tex}',
+                    fr'\title{{{course_name}}}',
+                    r'\begin{document}',
+                    r'    \maketitle',
+                    r'    \tableofcontents',
+                ]
+                file.write('\n'.join(lines))
+                note_names = await self.get_note_names(course_name)
+                for note_name in note_names:
+                    print(f"Writing note {note_name}...")
+                    note_content = await self.get_note_content(course_name, note_name)
+                    file.write(note_content + "\n")
+                file.write(r'\end{document}')
+
+            figure_names = await self.get_figure_names(course_name)
+            for figure_name in figure_names:
+                print(f"Getting figure {figure_name}")
+                figure_content = await self.get_figure_content(course_name, figure_name)
+                with open(f'../notes/{course_name}/figures/{figure_name}', 'wb') as file:
+                    file.write(figure_content)
